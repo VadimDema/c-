645dfe193bd82440165072d486ed9f0b11f2e65d
VadimDema <90608445+VadimDema@users.noreply.github.com>
2021-09-27 13:10:59 +0300

    Add files via upload

---
+++ тест/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static bool validatePassword(string password)
+        {
+            return true;
+        }
+    } 
+        
+}
+
